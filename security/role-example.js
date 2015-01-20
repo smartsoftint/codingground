4c32750f8584164382fbdf6eb3abab43bb15a331
smartsoftint <asegreda@smartsoftint.com>
2015-01-19 20:17:31 -0600

    Created by Coding Ground Client

---
+++ security/role-example.js
@@ -0,0 +1,10 @@
+{
+    "urid"   : "urid-143434343",
+    "nombre" : "TESTrole",
+    "description" : "este role es de prueba",
+    "uen" : "uen-001",
+    "priority": 4,
+    "permissions":{
+    }
+    
+}
