fded396a28f990098c1dda3563fa648caf72ae85
smartsoftint <asegreda@smartsoftint.com>
2015-01-19 20:10:15 -0600

    Created by Coding Ground Client

---
+++ security/user-example.js
@@ -0,0 +1,22 @@
+{
+    "uuid"     : "UUID-adadadadad",
+    "nickName" : "andres",
+    "domain"   : "segreda.com",
+    "active"   : true,
+    "userName" : "Juan Andres Segreda Johanning",
+    "departament" : "DPID",
+    "uuidSup" : "",
+    "email"   : "andres@segreda.com",
+    "tel"     : "87065014 ext107",
+    "photo"   : "https://s3.imga.html",
+    "UENs"     : ["UEN-001","UEN-003"] ,  
+    "roles"    : ["URID-001","UTID-023"],
+    "passwords": ["ekmslfmslm","fslfslfmslmf","309difi4304"],
+    "expirate" : 10920920493,
+    "expMotive" : 1,
+    
+    
+    "permissions" : {
+    }
+    
+}
